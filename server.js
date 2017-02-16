58ba1682fb1b8c465a888d09014f4cbac900af3b
Ali Hekimoglu <Ali Hekimoglu>
2017-02-16 22:29:37 +0300

    added http get support. implemented getById and list methods. general improvements.

--- server.js
+++ server.js
@@ -23,66 +23,87 @@ mongoose.connection.on('disconnected', () => {
     loggerInstance.logInfo('db connection is lost!');
 });
 
-var saveCall = function (res, model) {
-    model.save((err) => {
-        if (err) {
-            loggerInstance.logError(err);
-            res.status(500).send(JSON.stringify(err));
-        } else {
-            loggerInstance.logInfo('object created successfully');
-            loggerInstance.logInfo(model);
-            res.send(JSON.stringify(model));
-        }
-    })
-}
 
+function finalize(err, obj, res) {
+    if (err) {
+        loggerInstance.logException(err);
+        res.status(500).send(JSON.stringify(err))
+    } else {
+        res.send(obj);
+    }    
+}
 
 var Income = require('./data/income');
 
 //todo move this to another module!
 var incomeApiMethods = [{
-    userFriendlyName: 'Create New Income',
-    methodName: '/incomes/create',
-    httpRequestType: 'post',
-    parameters: [{
-        objName: 'name',
-        reqName: 'name',
-        type: 'value'
-    }, {
-        objName: 'amount',
-        reqName: 'amount',
-        type: 'value'
-    }],
-    execute: function (req, res, next, i = 0) {
+        userFriendlyName: 'Get Income By Id',
+        methodName: '/incomes/byId/:id',
+        httpRequestType: 'get',
+        execute: function (req, res, next, i = 0) {
+            var id = req.params.id;
+            Income.findById(id, function (err, income) {
+                finalize(err, income, res);
+            });
+        }
+    },
+    {
+        userFriendlyName: 'Get All Incomes',
+        methodName: '/incomes/list',
+        httpRequestType: 'get',
+        execute: function (req, res, next, i = 1) {
+            Income.find({}, function (err, incomes) {
+                finalize(err, incomes, res);
+            });
+        }
+    },
+    {
+        userFriendlyName: 'Create New Income',
+        methodName: '/incomes/create',
+        httpRequestType: 'post',
+        parameters: [{
+            objName: 'name',
+            reqName: 'name',
+            type: 'value'
+        }, {
+            objName: 'amount',
+            reqName: 'amount',
+            type: 'value'
+        }],
+        execute: function (req, res, next, i = 2) {
 
-        var parameters = incomeApiMethods[0].parameters;
-        objectBuilder.init();
-        parameters.forEach(function (element) {
-            if (element.type === 'value') {
-                objectBuilder.add(element.objName, req.body[element.reqName]);
-            } else if (element.type === 'object') {
-                loggerInstance.logInfo('object builder does not know how to add new child object yet!');
-            } else {
-                loggerInstance.logException('not know parameter type');
-            }
-        }, this);
+            var parameters = incomeApiMethods[i].parameters;
+            objectBuilder.init();
+            parameters.forEach(function (element) {
+                if (element.type === 'value') {
+                    objectBuilder.add(element.objName, req.body[element.reqName]);
+                } else if (element.type === 'object') {
+                    loggerInstance.logInfo('object builder does not know how to add new child object yet!');
+                } else {
+                    loggerInstance.logException('not know parameter type');
+                }
+            }, this);
 
-        var incomeData = objectBuilder.finalize();
-        var income = new Income(incomeData);
-        saveCall(res, income);
+            var incomeData = objectBuilder.finalize();
+            var income = new Income(incomeData)
+            income.save(function (err) {
+                finalize(err, income, res)
+            });
+        }
     }
-}];
+];
 
 incomeApiMethods.forEach(function (element) {
-
     if (element.httpRequestType === 'get') {
-        loggerInstance.logInfo('get is not implemented')
+        app.get(element.methodName, element.execute);
+        loggerInstance.logInfo('Http Method Registered')
+        loggerInstance.logInfo(element);
     } else if (element.httpRequestType === 'post') {
         app.post(element.methodName, element.execute);
         loggerInstance.logInfo('Http Method Registered');
         loggerInstance.logInfo(element);
     } else {
-        loggerInstance.logInfo('Unknown http method type');
+        loggerInstance.logError('Unknown http method type');
     }
 }, this);
 
